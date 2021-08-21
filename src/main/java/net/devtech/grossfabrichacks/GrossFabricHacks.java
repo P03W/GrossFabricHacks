3c894f94f3b56825f94324eaff4a5d666f2159f1
P03W <P03W@users.noreply.github.com>
2021-08-20 23:20:14 -0400

    Remove instrumentation, unsafe loader, minify license

--- src/main/java/net/devtech/grossfabrichacks/GrossFabricHacks.java
+++ src/main/java/net/devtech/grossfabrichacks/GrossFabricHacks.java
@@ -1,22 +1,17 @@
 package net.devtech.grossfabrichacks;
 
-import net.devtech.grossfabrichacks.entrypoints.TransformerRegistrar;
 import net.devtech.grossfabrichacks.transformer.asm.AsmClassTransformer;
 import net.devtech.grossfabrichacks.transformer.asm.RawClassTransformer;
 import net.devtech.grossfabrichacks.unsafe.UnsafeUtil;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.LanguageAdapter;
-import net.fabricmc.loader.launch.knot.UnsafeKnotClassLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import user11681.dynamicentry.DynamicEntry;
 
 import java.io.InputStream;
 
 public class GrossFabricHacks implements LanguageAdapter {
     private static final Logger LOGGER = LogManager.getLogger("GrossFabricHacks");
-
-    public static final UnsafeKnotClassLoader UNSAFE_LOADER;
 
     @Override
     public native <T> T create(net.fabricmc.loader.api.ModContainer mod, String value, Class<T> type);
@@ -38,7 +33,7 @@ public class GrossFabricHacks implements LanguageAdapter {
     }
 
     static {
-        LOGGER.info("no good? no, this man is definitely up to evil.");
+        LOGGER.info("Fabric Mass ASM is Starting");
 
         try {
             final ClassLoader applicationClassLoader = FabricLoader.class.getClassLoader();
@@ -46,8 +41,6 @@ public class GrossFabricHacks implements LanguageAdapter {
 
             final String[] classes = {
                 "net.gudenau.lib.unsafe.Unsafe",
-                "net.devtech.grossfabrichacks.instrumentation.InstrumentationAgent",
-                "net.devtech.grossfabrichacks.instrumentation.InstrumentationApi",
                 "net.devtech.grossfabrichacks.GrossFabricHacks$State",
                 "net.devtech.grossfabrichacks.unsafe.UnsafeUtil",
                 "net.devtech.grossfabrichacks.unsafe.UnsafeUtil$FirstInt"
@@ -66,10 +59,6 @@ public class GrossFabricHacks implements LanguageAdapter {
 
                 UnsafeUtil.defineClass(name, bytecode, applicationClassLoader, GrossFabricHacks.class.getProtectionDomain());
             }
-
-            LOGGER.warn("KnotClassLoader, you fool! Loading me was a grave mistake.");
-            
-            UNSAFE_LOADER = UnsafeUtil.defineAndInitializeAndUnsafeCast(KnotClassLoader, "net.fabricmc.loader.launch.knot.UnsafeKnotClassLoader", KnotClassLoader.getClass().getClassLoader());
         } catch (final Throwable throwable) {
             throw new RuntimeException(throwable);
         }
